e2165d57bffa7e71d5edc485a240517a1be9dc56
Ben Morris <ben@bendmorris.com>
2016-10-31 00:04:52 -0700

    Add macro to preserve 'com.haxepunk.*' imports with a deprecation warning.

--- haxepunk/utils/Platform.hx
+++ haxepunk/utils/Platform.hx
@@ -4,15 +4,24 @@ package haxepunk.utils;
 import haxe.macro.Compiler;
 import haxe.macro.Context;
 
+@:dox(hide)
 class Platform
 {
 	static function run()
 	{
-		if (Context.defined("flash")) {}
-		else
+		Context.onTypeNotFound(function (typeName:String)
 		{
-			Compiler.define("tile_shader");
-		}
+			var parts = typeName.split(".");
+			if (parts[0] == "com" && parts[1] == "haxepunk")
+			{
+				var name = parts[parts.length - 1],
+					currentPack = parts.slice(1, parts.length - 1),
+					deprecatedPack = parts.slice(0, parts.length - 1);
+				trace('Warning: the com.haxepunk package is deprecated ($typeName -> ' + currentPack.join(".") + '.$name)');
+				return {name: name, pack: deprecatedPack, kind: TDAlias(TPath({name: name, pack: currentPack})), fields: [], pos: Context.currentPos()};
+			}
+			return null;
+		});
 	}
 }
 #end
